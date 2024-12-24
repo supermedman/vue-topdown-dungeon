d6455ab8f7a236f64ee787d5c62d54236ee251c1
Th3Ward3n <liam.hatton@gmail.com>
2024-12-23 20:08:51 -0500

    Improved Level gen option menu styling

--- src/components/MapMakerControl.vue
+++ src/components/MapMakerControl.vue
@@ -25,34 +25,39 @@ export default defineComponent({
 </script>
 
 <template>
-    <div id="level-gen-options">
-        <div :hidden="levelLoaded">
-            <input type="radio" id="dimOne" name="dimOption" :value="8" checked
-            v-model.number="dimValue"
-            @change="$emit('updateDimension', dimValue)"
-            />
-            <label for="dimOne">8x8</label>
+    <div id="level-gen-container">
+        <div :hidden="levelLoaded" class="level-gen-header">
+            <h3>Dimensions</h3>
         </div>
-        <div :hidden="levelLoaded">
-            <input type="radio" id="dimTwo" name="dimOption" :value="10" 
-            v-model.number="dimValue"
-            @change="$emit('updateDimension', dimValue)"
-            />
-            <label for="dimTwo">10x10</label>
-        </div>
-        <div :hidden="levelLoaded">
-            <input type="radio" id="dimThree" name="dimOption" :value="12" 
-            v-model.number="dimValue"
-            @change="$emit('updateDimension', dimValue)"
-            />
-            <label for="dimThree">12x12</label>
-        </div>
-        <div :hidden="levelLoaded">
-            <input type="radio" id="dimFour" name="dimOption" :value="16" 
-            v-model.number="dimValue"
-            @change="$emit('updateDimension', dimValue)"
-            />
-            <label for="dimFour">16x16</label>
+        <div class="level-gen-options">
+            <label :hidden="levelLoaded" class="option-label-container" for="dimOne">8x8
+                <input :hidden="levelLoaded" type="radio" id="dimOne" name="dimOption" :value="8" checked
+                v-model.number="dimValue"
+                @change="$emit('updateDimension', dimValue)"
+                />
+                <span class="option-selector"></span>
+            </label>
+            <label :hidden="levelLoaded" class="option-label-container" for="dimTwo">10x10
+                <input type="radio" id="dimTwo" name="dimOption" :value="10" 
+                v-model.number="dimValue"
+                @change="$emit('updateDimension', dimValue)"
+                />
+                <span class="option-selector"></span>
+            </label>
+            <label :hidden="levelLoaded" class="option-label-container" for="dimThree">12x12
+                <input type="radio" id="dimThree" name="dimOption" :value="12" 
+                v-model.number="dimValue"
+                @change="$emit('updateDimension', dimValue)"
+                />
+                <span class="option-selector"></span>
+            </label>
+            <label :hidden="levelLoaded" class="option-label-container" for="dimFour">16x16
+                <input type="radio" id="dimFour" name="dimOption" :value="16" 
+                v-model.number="dimValue"
+                @change="$emit('updateDimension', dimValue)"
+                />
+                <span class="option-selector"></span>
+            </label>
         </div>
     </div>
 </template>
